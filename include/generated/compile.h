89f29790ec903dc827ee4332b520b444e8776b66
jingjie.hei <jingjie.hei@archermind.com>
2015-05-29 14:06:36 +0800

    Source code

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 2 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 Fri Oct 31 09:40:11 CST 2014"
+#define LINUX_COMPILE_BY "archermind"
+#define LINUX_COMPILE_HOST "cdz140150"
+#define LINUX_COMPILER "gcc version 4.3.2 (crosstool-NG-1.8.1-none) "
